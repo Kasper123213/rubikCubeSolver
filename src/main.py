c97567983a0124cd2362818c990dc0808a97f3dd
Kasper Radom <radom.kasper@gmail.com>
2024-02-07 04:02:34 +0100

    Podstawy

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print(30)
+from cube import Cube
+
+def main():
+    cube = Cube()
+    cube.printCube()
+
+    print("\n"*3)
+
+    cube.bMove()
+    cube.printCube()
+
+    print("\n"*3)
+
+    cube.rotateSurface(1, True)
+    cube.printCube()
+
+    print("\n"*3)
+
+    cube.rotateSurface(1, False)
+    cube.printCube()
+
+
+if __name__ == "__main__":
+    main()
